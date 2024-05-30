17c2173e9228880e64429504be3c6d54439ceea5
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2024-05-29 23:07:08 -0500

    Add button to change the dimensions of the grid.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -8,4 +9,39 @@ for (let i = 0; i < 16; i++) {
         row.appendChild(child);
     }
     container.appendChild(row);
+}
+
+const btn = document.querySelector("#changegrid");
+btn.addEventListener("click", setGrid);
+
+function setGrid() {
+    const userSetting = parseInt(prompt("Please enter the number of squares (between 2 and 100) that you would like going down and across for your sketch pad."));
+    if (userSetting < 2 || userSetting > 100) {
+        alert("The number you entered is too small or too large. Please try again and enter a number between 2 and 100.");
+        return;
+    }
+    // clear existing grid
+    clearGrid();
+    createGrid(userSetting);
+
+}
+
+function createGrid(numSquares) {
+    for (let i = 0; i < numSquares; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let i = 0; i < numSquares; i++) {
+            const child = document.createElement("div");
+            child.classList.add("child");
+            row.appendChild(child);
+        }
+        container.appendChild(row);
+    }
+}
+
+function clearGrid() {
+    const container = document.querySelector(".container");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
 }
